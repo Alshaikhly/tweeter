5e9bb88b52eb05ff342c5275aebd7a6e43d1be93
Alshaikhly <alshaikhly89@gmail.com>
2020-06-18 00:04:48 +0000

    form validation & refetch tweet on submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,28 +1,28 @@
 
- let tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+//  let tweetData = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ]
 
  function createTweetElement(tweet) {
   // use variables to refactor code!
@@ -51,7 +51,7 @@ const renderTweets = function(tweets) {
   
   for (const tweet of tweets) {
     let $tweetElement = createTweetElement(tweet);
-    $('#tweets-container').append($tweetElement);
+    $('#tweets-container').prepend($tweetElement);
   }
 }
 
@@ -64,13 +64,30 @@ $(document).ready(function () {
     event.preventDefault();
 
     const data = $(this).serialize()
+    const $tweetLength = $('#tweet-text').val().length;
+    const onlySpaces = $('#tweet-text').val().replace(/ /g,'')
+    // or const $tweetLength = data.split('=')[1].length
 
+    if ($tweetLength <= 0 || $tweetLength > 140 || onlySpaces === '') {
+      alert('Please post an acceptable tweet length')
+    } else {
     $.post('/tweets',data)
-    .then(function(_data) {
-      console.log("received >>", _data);
-    })
+    .then(function(data) {
+      console.log("received >>", data);
+      loadtweets()
+    })}
   });
 
-  renderTweets(tweetData);
+  const loadtweets = () => {
+    $.getJSON('/tweets')
+      .then(function(data) {
+        console.log('data received >>', data);
 
+        $('#tweets-container').empty()
+
+        renderTweets(data);
+      })
+  }
+  
+  loadtweets()
 });
